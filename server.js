4f2a96034139c2e2797a2175b8cda6ed71399df4
Romulo Soliz <ripzier@gmail.com>
2019-07-18 09:33:10 -0500

    Refactored server

--- server.js
+++ server.js
@@ -4,27 +4,25 @@ const mongoose = require('mongoose')
 const authors = require('./routes/authors')
 const books = require('./routes/books')
 
+const port = 5000
 const mongo_uri = 'mongodb://localhost:27017/mvc'
 
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
-    console.log(`Successfully connected to ${mongo_uri}`)
+    console.log(`Conectado a la base de datos: ${mongo_uri}`)
   })
   .catch(err => {
-    console.log('Error connecting to database')
-    console.log('Ending the application now...')
+    console.log('Error al conectarse a la base de datos')
+    console.log('Terminando la aplicación ahora...')
     process.exit()
   })
 
 const app = express()
 
-// Lee el contenido del cuerpo de las peticiones como json
 app.use(express.json())
-
-// Rutas
 app.use('/api/autores', authors)
 app.use('/api/libros', books)
 
-app.listen(5000, () => {
-  console.log('El servidor está escuchando en el puerto 5000')
+app.listen({ port }, () => {
+  console.log(`Servidor listo en http://localhost:${port}`)
 })
