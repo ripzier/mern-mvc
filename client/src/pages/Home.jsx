f2f7a8d65de3a092aed174b9402a27f5d1c3f408
Romulo Soliz <ripzier@gmail.com>
2019-07-18 10:46:24 -0500

    Updated Home page to use graphql api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,69 @@
+import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider, Query } from 'react-apollo'
 import React from 'react'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Item, Segment } from 'semantic-ui-react'
 
 const Home = () => {
+  const client = new ApolloClient()
+
   return (
     <>
       <Header as='h1' textAlign='center'>
-        MVC con Express, React y MongoDB
+        Catálogo de libros
       </Header>
       <Container text textAlign='justified'>
-        Esta página es un demo de la construcción de una aplicación web con
-        arquitectura MVC, donde el componente de modelos está hecho en MongoDB,
-        el componente de vistas está hecho en React, y el componente de
-        controlodares está hecho en Express.js.
+        <Segment>
+          <ApolloProvider client={client}>
+            <Query
+              query={gql`
+                {
+                  books {
+                    id
+                    title
+                    genre
+                    publicationDate
+                    author {
+                      givenName
+                      lastName
+                      books {
+                        id
+                        title
+                      }
+                    }
+                  }
+                }
+              `}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>
+                if (error) return <p>Error :(</p>
+
+                return (
+                  <Item.Group>
+                    {data.books.map(
+                      ({ id, title, genre, publicationDate, author }) => (
+                        <Item key={id}>
+                          <Item.Content>
+                            <Item.Header>{title}</Item.Header>
+                            <Item.Meta>{`Género: ${genre}`}</Item.Meta>
+                            <Item.Meta>{`Autor: ${author.givenName} ${
+                              author.lastName
+                            }`}</Item.Meta>
+                            <Item.Meta>{`Fecha de Publicación: ${publicationDate}`}</Item.Meta>
+                            <Item.Extra>{`Otros libros del autor | ${author.books
+                              .filter(book => book.id !== id)
+                              .map(book => book.title)
+                              .join(' | ')} |`}</Item.Extra>
+                          </Item.Content>
+                        </Item>
+                      )
+                    )}
+                  </Item.Group>
+                )
+              }}
+            </Query>
+          </ApolloProvider>
+        </Segment>
       </Container>
     </>
   )
