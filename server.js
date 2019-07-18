9c827abcc75ab52a93ae3b890c9f168cfa00f0f6
Romulo Soliz <ripzier@gmail.com>
2019-07-18 10:40:00 -0500

    Integrated graphql to express server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
 const mongoose = require('mongoose')
 
 const authors = require('./routes/authors')
 const books = require('./routes/books')
+const { typeDefs, resolvers } = require('./graphql/schema')
 
 const port = 5000
 const mongo_uri = 'mongodb://localhost:27017/mvc'
@@ -17,8 +19,10 @@ mongoose.connect(mongo_uri, { useNewUrlParser: true, useFindAndModify: false })
     process.exit()
   })
 
+const server = new ApolloServer({ typeDefs, resolvers })
 const app = express()
 
+server.applyMiddleware({ app })
 app.use(express.json())
 app.use('/api/autores', authors)
 app.use('/api/libros', books)
