7132eec90aaf95a462bec8bc368ac93df59a27c9
Romulo Soliz <ripzier@gmail.com>
2019-07-18 09:23:47 -0500

    Created controllers for the models

---
+++ controllers/author.js
@@ -0,0 +1,58 @@
+const Author = require('../models/Author')
+
+exports.list = (req, res) => {
+  Author.find()
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
+
+exports.details = (req, res) => {
+  Author.findById(req.params._id)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
+
+exports.create = (req, res) => {
+  const newAuthor = new Author(req.body)
+  newAuthor.save()
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+
+}
+
+exports.update = (req, res) => {
+  Author.findByIdAndUpdate(req.params._id, req.body)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
+
+exports.delete = (req, res) => {
+  Author.findByIdAndRemove(req.params._id)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
---
+++ controllers/book.js
@@ -0,0 +1,58 @@
+const Book = require('../models/Book')
+
+exports.list = (req, res) => {
+  Book.find().populate('author')
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
+
+exports.details = (req, res) => {
+  Book.findById(req.params._id)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
+
+exports.create = (req, res) => {
+  const newBook = new Book(req.body)
+  newBook.save()
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+
+}
+
+exports.update = (req, res) => {
+  Book.findByIdAndUpdate(req.params._id, req.body)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
+
+exports.delete = (req, res) => {
+  Book.findByIdAndRemove(req.params._id)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ message: 'Something went wrong' })
+    })
+}
