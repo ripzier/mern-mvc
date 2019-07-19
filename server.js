a1fcff5e3c5314d58fa32aadeedbc5297e48e7e1
Romulo Soliz <ripzier@gmail.com>
2019-07-18 19:23:17 -0500

    Update project for heroku deploy

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const authors = require('./routes/authors')
 const books = require('./routes/books')
 const { typeDefs, resolvers } = require('./graphql/schema')
 
-const port = 5000
-const mongo_uri = 'mongodb://localhost:27017/mvc'
+const port = process.env.PORT || 5000
+// const mongo_uri = 'mongodb://localhost:27017/mvc'
+const mongo_uri = 'mongodb+srv://dba:3w2U$^!#lL5T@maincluster-gmc7n.mongodb.net/mern-mvc?retryWrites=true&w=majority'
 
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
