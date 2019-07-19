7f4aa60a69a56f4d55d05eb80d857d45c306153a
Romulo Soliz <ripzier@gmail.com>
2019-07-18 19:32:40 -0500

    DB connection debuggin

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const { typeDefs, resolvers } = require('./graphql/schema')
 
 const port = process.env.PORT || 5000
 // const mongo_uri = 'mongodb://localhost:27017/mvc'
-const mongo_uri = 'mongodb+srv://dba:3w2U$^!#lL5T@maincluster-gmc7n.mongodb.net/mern-mvc?retryWrites=true&w=majority'
+const mongo_uri = 'mongodb+srv://dba:3w2U$^!#lL5T@maincluster-gmc7n.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
     console.log(`Conectado a la base de datos: ${mongo_uri}`)
   })
   .catch(err => {
+    console.log(err)
     console.log('Error al conectarse a la base de datos')
     console.log('Terminando la aplicación ahora...')
     process.exit()
